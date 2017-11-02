3d4455e7c56cb1b9c97d04ce8cd1d8fd81b293db
Аркадий Безгерц <bezgerts@gmail.com>
2017-11-02 18:10:07 +0300

    Корректировка SpringConfig и PechkinApplication.

--- src/main/java/me/bezgerts/pechkin/PechkinApplication.java
+++ src/main/java/me/bezgerts/pechkin/PechkinApplication.java
@@ -2,11 +2,9 @@ package me.bezgerts.pechkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@EnableOAuth2Sso
 @ImportResource("classpath:beans.xml")
 public class PechkinApplication {
     public static void main(String[] args) {
--- src/main/java/me/bezgerts/pechkin/config/SecurityConfig.java
+++ src/main/java/me/bezgerts/pechkin/config/SecurityConfig.java
@@ -1,10 +1,42 @@
 package me.bezgerts.pechkin.config;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableOAuth2Sso
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user").password("user").roles("USER").build());
+        manager.createUser(User.withUsername("admin").password("admin").roles("ADMIN").build());
+        return manager;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/",
+                             "/login**",
+                             "/webjars/**",
+                             "/resources/**",
+                             "/css/**",
+                             "/js/**",
+                             "/about")
+                .permitAll()
+                .antMatchers("/admin/**").hasAnyRole("ADMIN")
+                .anyRequest()
+                .authenticated();
+    }
 }
+
